31f30534da9e306d46b1d8fb17a08ec3cf12d51f
Shreyansh Das <shreyanshdas00@gmail.com>
2020-10-17 13:12:36 +0530

    Update BERT_embedding.py

--- BERT_embedding.py
+++ BERT_embedding.py
@@ -1,25 +1,29 @@
 #importing the pre-trained BERT embeddings model
 from transformers import AutoTokenizer, AutoModelForMaskedLM
 
-tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
+class BERT():
+  def __init__(self, list_input):
+    self.__tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
 
-model = AutoModelForMaskedLM.from_pretrained("bert-base-uncased")
+    self.__model = AutoModelForMaskedLM.from_pretrained("bert-base-uncased")
 
-model.to('cuda')
+    self.__model.to('cuda')
+    
+    return self.get_embedding(self, list_input)
 
 
-#utility function to extract word embeddings for a given utterance
-def get_embedding(list_input):
-  
-  em_words=[]
-  for question in list_input:
-    ids=tokenizer.encode(question)
-    ids = torch.LongTensor(ids).unsqueeze(0)
-    em_words.append(ids)
-  embeddings=[]
-  with torch.no_grad():
-    for x in em_words:
-      x=x.to(device)
-      embeddings.append((model(input_ids=x)[2])[-1])
-  return embeddings
+  #utility function to extract word embeddings for a given utterance
+  def get_embedding(self, list_input):
+
+    em_words=[]
+    for question in list_input:
+      ids=self.__tokenizer.encode(question)
+      ids = torch.LongTensor(ids).unsqueeze(0)
+      em_words.append(ids)
+    embeddings=[]
+    with torch.no_grad():
+      for x in em_words:
+        x=x.to(device)
+        embeddings.append((self.__model(input_ids=x)[2])[-1])
+    return embeddings
 
