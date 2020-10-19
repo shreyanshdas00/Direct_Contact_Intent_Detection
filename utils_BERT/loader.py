3d34358bc46d2957348bbc5c63950e33c8da7438
Shreyansh Das <shreyanshdas00@gmail.com>
2020-10-19 14:10:16 +0530

    Update loader.py

--- utils_BERT/loader.py
+++ utils_BERT/loader.py
@@ -8,6 +8,125 @@ from ordered_set import OrderedSet
 from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
 
+
+class Alphabet(object):
+    """
+    Storage and serialization a set of elements.
+    """
+
+    def __init__(self, name, if_use_pad, if_use_unk):
+
+        self.__name = name
+        self.__if_use_pad = if_use_pad
+        self.__if_use_unk = if_use_unk
+
+        self.__index2instance = OrderedSet()
+        self.__instance2index = OrderedDict()
+
+        self.__counter = Counter()
+
+        if if_use_pad:
+            self.__sign_pad = "<PAD>"
+            self.add_instance(self.__sign_pad)
+        if if_use_unk:
+            self.__sign_unk = "<UNK>"
+            self.add_instance(self.__sign_unk)
+
+    def name(self):
+        return self.__name
+
+    def add_instance(self, instance):
+        """ Add instances to alphabet.
+        1, We support any iterative data structure which
+        contains elements of str type.
+        2, We will count added instances that will influence
+        the serialization of unknown instance.
+        :param instance: is given instance or a list of it.
+        """
+
+        if isinstance(instance, (list, tuple)):
+            for element in instance:
+                self.add_instance(element)
+            return
+
+        # We only support elements of str type.
+        assert isinstance(instance, str)
+
+        # count the frequency of instances.
+        self.__counter[instance] += 1
+
+        if instance not in self.__index2instance:
+            self.__instance2index[instance] = len(self.__index2instance)
+            self.__index2instance.append(instance)
+
+    def get_index(self, instance):
+        """ Serialize given instance and return.
+        For unknown words, the return index of alphabet
+        depends on variable self.__use_unk:
+            1, If True, then return the index of "<UNK>";
+            2, If False, then return the index of the
+            element that hold max frequency in training data.
+        :param instance: is given instance or a list of it.
+        :return: is the serialization of query instance.
+        """
+
+        if isinstance(instance, (list, tuple)):
+            return [self.get_index(elem) for elem in instance]
+
+        assert isinstance(instance, str)
+
+        try:
+            return self.__instance2index[instance]
+        except KeyError:
+            if self.__if_use_unk:
+                return self.__instance2index[self.__sign_unk]
+            else:
+                max_freq_item = self.__counter.most_common(1)[0][0]
+                return self.__instance2index[max_freq_item]
+
+    def get_instance(self, index):
+        """ Get corresponding instance of query index.
+        if index is invalid, then throws exception.
+        :param index: is query index, possibly iterable.
+        :return: is corresponding instance.
+        """
+
+        if isinstance(index, list):
+            return [self.get_instance(elem) for elem in index]
+
+        return self.__index2instance[index]
+
+    def save_content(self, dir_path):
+        """ Save the content of alphabet to files.
+        There are two kinds of saved files:
+            1, The first is a list file, elements are
+            sorted by the frequency of occurrence.
+            2, The second is a dictionary file, elements
+            are sorted by it serialized index.
+        :param dir_path: is the directory path to save object.
+        """
+
+        # Check if dir_path exists.
+        if not os.path.exists(dir_path):
+            os.mkdir(dir_path)
+
+        list_path = os.path.join(dir_path, self.__name + "_list.txt")
+        with open(list_path, 'w') as fw:
+            for element, frequency in self.__counter.most_common():
+                fw.write(element + '\t' + str(frequency) + '\n')
+
+        dict_path = os.path.join(dir_path, self.__name + "_dict.txt")
+        with open(dict_path, 'w') as fw:
+            for index, element in enumerate(self.__index2instance):
+                fw.write(element + '\t' + str(index) + '\n')
+
+    def __len__(self):
+        return len(self.__index2instance)
+
+    def __str__(self):
+        return 'Alphabet {} contains about {} words: \n\t{}'.format(self.name, len(self), self.__index2instance)
+
+
 class TorchDataset(Dataset):
     """
     Helper class implementing torch.utils.data.Dataset to
@@ -31,9 +150,32 @@ class TorchDataset(Dataset):
 class DatasetManager(object):
 
     def __init__(self, args):
+
+        # Instantiate alphabet objects.
+        self.__word_alphabet = Alphabet('word', if_use_pad=True, if_use_unk=True)
+        self.__intent_alphabet = Alphabet('intent', if_use_pad=False, if_use_unk=False)
+
+        # Record the raw text of dataset.
+        self.__text_word_data = {}
+        self.__text_intent_data = {}
+
+        # Record the serialization of dataset.
+        self.__digit_word_data = {}
+        self.__digit_intent_data = {}
+
         self.__args = args
-        self.__text = []
-        self.__intent = []
+
+    @property
+    def test_sentence(self):
+        return deepcopy(self.__text_word_data['test'])
+
+    @property
+    def word_alphabet(self):
+        return deepcopy(self.__word_alphabet)
+
+    @property
+    def intent_alphabet(self):
+        return deepcopy(self.__intent_alphabet)
 
     @property
     def num_epoch(self):
@@ -96,6 +238,10 @@ class DatasetManager(object):
         if not os.path.exists(self.save_dir):
             os.mkdir(self.save_dir)
 
+        alphabet_dir = os.path.join(self.__args.save_dir, "alphabet")
+        self.__word_alphabet.save_content(alphabet_dir)
+        self.__intent_alphabet.save_content(alphabet_dir)
+
     def get_dataset(self, data_name, is_digital):
         """ Get dataset of given unique name.
         :param data_name: is name of stored dataset.
@@ -103,15 +249,28 @@ class DatasetManager(object):
         :return: the required dataset.
         """
 
-        return self.__text[data_name], \
-                   self.__intent[data_name]
+        if is_digital:
+            return self.__digit_word_data[data_name], \
+                   self.__digit_intent_data[data_name]
+        else:
+            return self.__text_word_data[data_name], \
+                   self.__text_intent_data[data_name]
 
     def add_file(self, file_path, data_name, if_train_file):
         text, intent = self.__read_file(file_path)
 
+        if if_train_file:
+            self.__word_alphabet.add_instance(text)
+            self.__intent_alphabet.add_instance(intent)
+
         # Record the raw text of dataset.
-        self.__text[data_name] = text
-        self.__text[data_name] = intent
+        self.__text_word_data[data_name] = text
+        self.__text_intent_data[data_name] = intent
+
+        # Serialize raw text and stored it.
+        self.__digit_word_data[data_name] = self.__word_alphabet.get_index(text)
+        if if_train_file:
+            self.__digit_intent_data[data_name] = self.__intent_alphabet.get_index(intent)
 
     @staticmethod
     def __read_file(file_path):
@@ -125,7 +284,7 @@ class DatasetManager(object):
 
         with open(file_path, 'r') as fr:
             for line in fr.readlines():
-                items = line.split()
+                items = line.strip().split()
 
                 if len(items) == 1:
                     texts.append(text)
@@ -135,7 +294,7 @@ class DatasetManager(object):
                     text = []
 
                 elif len(items) == 2:
-                    text+=" "+items[0]
+                    text.append(items[0].strip())
 
         return texts, intents
 
@@ -143,11 +302,50 @@ class DatasetManager(object):
         if batch_size is None:
             batch_size = self.batch_size
 
-        text = self.__text[data_name]
-        intent = self.__intent[data_name]
+        if is_digital:
+            text = self.__digit_word_data[data_name]
+            intent = self.__digit_intent_data[data_name]
+        else:
+            text = self.__text_word_data[data_name]
+            intent = self.__text_intent_data[data_name]
         dataset = TorchDataset(text, intent)
 
         return DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, collate_fn=self.__collate_fn)
+
+    @staticmethod
+    def add_padding(texts, items=None, digital=True):
+        len_list = [len(text) for text in texts]
+        max_len = max(len_list)
+
+        # Get sorted index of len_list.
+        sorted_index = np.argsort(len_list)[::-1]
+
+        trans_texts, seq_lens, trans_items = [], [], None
+        if items is not None:
+            trans_items = [[] for _ in range(0, len(items))]
+
+        for index in sorted_index:
+            seq_lens.append(deepcopy(len_list[index]))
+            trans_texts.append(deepcopy(texts[index]))
+            if digital:
+                trans_texts[-1].extend([0] * (max_len - len_list[index]))
+            else:
+                trans_texts[-1].extend(['<PAD>'] * (max_len - len_list[index]))
+
+            # This required specific if padding after sorting.
+            if items is not None:
+                for item, (o_item, required) in zip(trans_items, items):
+                    item.append(deepcopy(o_item[index]))
+                    if required:
+                        if digital:
+                            item[-1].extend([0] * (max_len - len_list[index]))
+                        else:
+                            item[-1].extend(['<PAD>'] * (max_len - len_list[index]))
+
+        if items is not None:
+            return trans_texts, trans_items, seq_lens
+        else:
+            return trans_texts, seq_lens
 
     @staticmethod
     def __collate_fn(batch):
